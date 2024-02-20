8394d92f7e337c17b6fce17591156dd5e09f76fe
Aji-Diliyaer <aji.diliyaer@arundo.com>
2024-02-21 00:19:16 +0100

    make it pretty

--- src/test/java/cubecart/utils/SessionUtil.java
+++ src/test/java/cubecart/utils/SessionUtil.java
@@ -1,4 +1,30 @@
 package cubecart.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SessionUtil {
+
+    Map<String, Object> sessionData = new HashMap<>();
+    private static SessionUtil instance;
+
+    private SessionUtil(){
+    }
+
+    public static SessionUtil getInstance(){
+        if(SessionUtil.instance == null){
+            SessionUtil.instance = new SessionUtil();
+        }
+        return SessionUtil.instance;
+    }
+
+    public void setItem(String key, Object value){
+        sessionData.put(key,value);
+    }
+
+    public Object getItem(String key){
+        return sessionData.get(key);
+    }
+
+
 }
