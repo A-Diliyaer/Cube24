41b28b6cfc024bf5d0126ffbc701040b40197ac0
Babur101 <adrian_osmanli@hotmail.com>
2024-02-21 21:38:01 +0100

    ConfigReader

--- src/test/java/cubecart/utils/ConfigReader.java
+++ src/test/java/cubecart/utils/ConfigReader.java
@@ -1,4 +1,26 @@
 package cubecart.utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+    public static String readFromConfig(String fileName,String key){
+        Properties properties=new Properties();
+        //open the file in reading mode
+        FileInputStream inputStream= null;
+        try {
+            inputStream = new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String value= properties.getProperty(key);
+        return value;
+    }
 }
