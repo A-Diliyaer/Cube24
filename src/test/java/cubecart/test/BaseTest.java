58bfb953782fc90e8224939abb0b60e6160042f7
elxat <elxat999@yahoo.com>
2024-02-26 12:13:30 +0100

    Page object design

--- src/test/java/cubecart/test/BaseTest.java
+++ src/test/java/cubecart/test/BaseTest.java
@@ -2,8 +2,56 @@ package cubecart.test;
 
 import cubecart.utils.SessionUtil;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 
 public class BaseTest {
-    SessionUtil sessionUtil = SessionUtil.getInstance();
-    static Logger log = Logger.getLogger(BaseTest.class);
+
+    ChromeDriver driver;
+
+
+    public void setUp(){
+        System.out.println("Setting up resources for the test...");
+    }
+
+
+    public  void openBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://cubecartuat.unitedcoderapp.com/admin_d9nchr.php");
+    }
+
+
+
+    public  void login(String userName, String password) {
+        WebElement userNameField = driver.findElement(By.id("username"));
+        userNameField.sendKeys(userName);
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+        WebElement loginButton = driver.findElement(By.id("login"));
+        loginButton.click();
+    }
+
+    public  void logOut() {
+        WebElement logOutLink = driver.findElement(By.linkText("Log Out"));
+        logOutLink.click();
+    }
+
+    public  void closeBrowser() {
+        System.out.println("Tearing down resources after the test...");
+        driver.close();
+        driver.quit();
+    }
+
+
+
+
 }
+
+
+
