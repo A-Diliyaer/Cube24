a9af24fc56e3f26e4d28abcc59af835ec1d45f3c
A-Diliyaer <65502064+A-Diliyaer@users.noreply.github.com>
2024-03-03 10:42:36 +0100

    Update BaseTest.java

--- src/test/java/cubecart/test/BaseTest.java
+++ src/test/java/cubecart/test/BaseTest.java
@@ -15,18 +15,14 @@ public class BaseTest {
     public void setUp() {
         log.info("Starting Test Automation");
         log.info("Browser type ::"+ ConfigReader.getProperty("browser"));
-        log.info("Environment ::"+ConfigReader.getProperty("url"));
         String URL=ConfigReader.getProperty("url");
-        Driver.getDriver().manage().window().maximize();
-      String username = ConfigReader.getProperty("username");
-      String password = ConfigReader.getProperty("password");
+        log.info("Environment ::"+URL);
         Driver.getDriver().get(URL);
         log.info("Starting browser");
         UiUtil.getElement("//*[@id='username']").sendKeys(ConfigReader.getProperty("username"));
         UiUtil.getElement("//*[@id='password']").sendKeys(ConfigReader.getProperty("password"));
         UiUtil.ClickElement("//*[@id='login']");
         log.info("logging into app");
-
     }
 
 
