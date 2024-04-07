9b87512cfd6a58c03ebb896f890d3f39f46aedb4
Babur101 <124695405+Babur101@users.noreply.github.com>
2024-04-07 14:53:44 +0200

    XpahtUtil (#24)
    * DbUtil Database Connection
    
    * DbUtil Database Connection
    
    * DbUtil Database Connection
    
    * DbUtil Database Connection
    
    * Update .gitignore
    
    * Delete src/test/resources/config.properties
    
    * XpathUtil
    
    ---------
    
    Co-authored-by: A-Diliyaer <65502064+A-Diliyaer@users.noreply.github.com>

--- src/test/java/cubecart/utils/XpathUtil.java
+++ src/test/java/cubecart/utils/XpathUtil.java
@@ -8,6 +8,38 @@ public class XpathUtil {
     public static final String usernameField = "//*[@id='username']";
     public static final String passwordField = "//*[@id='password']";
     public static final String loginButton = "//*[@id='login']";
+
+
+//*************************************************************About Reviews***************************************************************************
+    //Reviews
+    public static final String reviews_menu = "//*[@id='menu_Inventory']//li//a[@href='?_g=products&node=reviews']";
+    public static final String product_Reviews = "//div[@id='tab_reviews']//a['Product Reviews']";
+    public static final String add_Review = "//*[@id='tab_control']//div[@id='tab_reviews']//a";
+    public static final String bulk_Delete = "//*[@id='tab_control']//div[@id='tab_bulk_delete']//a";
+    public static final String search = "//*[@id='tab_control']//div[@id='tab_search']//a";
+    //Add review
+    public static final String product_Field = "//*[@id='review']//div//span//input[@id='ajax_name']";
+    public static final String status_Checkbox = "//*[@id='review']//div//span/img";
+    public static final String name_Field = "//*[@id='review']//input[@id='ajax_name']";
+    public static final String email_Field = "//*[@id='review']//fieldset//div//span//input[@id='review_email']";
+    public static final String title_Field = "//*[@id='review']//fieldset//div//span//input[@id='review_title']";
+    public static final String content_Field = "//*[@id='review']//fieldset//div//span//textarea[@id='review_content']";
+    public static final String rating_Stars = "//*[@id='review']//fieldset//div//span//input[@value='3']";
+    public static final String add_Review_Submit_Button = "//*[@class='form_control']//input[@type='submit']";
+    //Bulk Delete
+    public static final String bdEmail_Field = "//*[@id='bulk_delete']//span//input[@id='email']";
+    public static final String bdIpAddress_Field = "//*[@id='bulk_delete']//div//span//input[@id='ip_address']";
+    public static final String bdSubmit_Button = "//*[@class='form_control']//input[@class='submit']";
+    //Search Reviews
+    public static final String keywords_Field = "//div[@id='search']//fieldset//span//input[@id='search-keywords']";
+    public static final String filter_By_Product_Field = "//div[@id='search']//fieldset//span//input[@name='filter[product_string]']";
+    public static final String status_Dropbox_All = "//div[@id='search']//fieldset//span//select[@name='filter[approved]']//option[1]";
+    public static final String status_Dropbox_Approved ="//div[@id='search']//fieldset//span//select[@name='filter[approved]']//option[@value='1']";
+    public static final String status_Dropbox_Unapproved ="//div[@id='search']//fieldset//span//select[@name='filter[approved]']//option[@value='0']";
+    public static final String search_Submit_Button ="//div[@id='page_content']//div[@class='form_control']//input[@type='submit']";
+
+//*************************************************************About Reviews***************************************************************************
+
     public static final String productsLink= "//*[@id='nav_products']";
     public static final String addProductLink="//*[text()='Add Product']";
     public static final String productNameField="//*[@id='name']";
@@ -37,10 +69,5 @@ public class XpathUtil {
     public static final String saveButton="//*[@value='Save']";
     public static final String successMessage="//*[text()='Product successfully created.']";
     public static final String logOutButton="//*[text()='Log Out ']";
-
-
-
-
-
 
 }
